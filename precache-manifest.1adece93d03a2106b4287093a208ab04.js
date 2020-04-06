0d20f3971c7f2fbe50ae7ec15a1582b847db0482
sbondar93 <sergeyb@xcritical.software>
2020-04-06 14:05:40 +0300

    Updates

---
+++ precache-manifest.1adece93d03a2106b4287093a208ab04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83492d2326b3e9b574cf524459450676",
+    "url": "/tabs/index.html"
+  },
+  {
+    "revision": "357cd839dfce505b7724",
+    "url": "/tabs/static/css/main.d8c8d18a.chunk.css"
+  },
+  {
+    "revision": "d358be1208a5709d5d7f",
+    "url": "/tabs/static/js/2.634db1cf.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/tabs/static/js/2.634db1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357cd839dfce505b7724",
+    "url": "/tabs/static/js/main.883b5fb7.chunk.js"
+  },
+  {
+    "revision": "5e5b686d289173536cb3",
+    "url": "/tabs/static/js/runtime-main.324d6537.js"
+  }
+]);
